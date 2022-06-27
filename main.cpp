e862d2001e3cf67b5ccee3827bccb62ffa13f4ee
Andrei Makarov <a.makarov@rdp.ru>
2022-06-27 15:23:35 +0300

    la::Mat class under development

--- main.cpp
+++ main.cpp
@@ -7,7 +7,49 @@ int main() {
 
   la::Mat<int> m(3, 4, 5);
 
-  std::cout << m;
+  std::cout << m << std::endl;
+  m.negate();
+  std::cout << m << std::endl;
+
+  la::Mat<int> n = la::Mat<int>::CreateEye(4, 4);
+  std::cout << n << std::endl;
+  n.at(0, 1) = 3;
+  n.at(0, 2) = 3;
+  n.at(0, 3) = 3;
+
+  n.at(1, 2) = 3;
+  n.at(1, 3) = 3;
+
+  n.at(2, 3) = 3;
+
+  std::cout << n << std::endl;
+
+  n.transpose();
+  std::cout << n << std::endl;
+
+  la::Mat<int> f(2, 3);
+  f.at(0, 0) = 1;
+  f.at(0, 1) = 2;
+  f.at(0, 2) = 3;
+
+  f.at(1, 0) = 1;
+  f.at(1, 1) = 2;
+  f.at(1, 2) = 3;
+
+  std::cout << f << std::endl;
+
+  la::Mat<int> tf(3, 2);
+
+  f.transpose(tf);
+  std::cout << tf << std::endl;
+
+  la::Mat<float> flmat(3, 3, 3.14);
+  std::cout << flmat << std::endl;
+
+  flmat.dump(std::cout);
+
+  std::string file_matrix{"float_mat.txt"};
+  flmat.dump(file_matrix);
 
   return 0;
 }
--- mat.h
+++ mat.h
@@ -1,7 +1,10 @@
 #ifndef MAT_H
 #define MAT_H
 
+#include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <type_traits>
 
 namespace la {
 
@@ -112,11 +115,56 @@ class Mat {
     for (int i = 0; i < quantity_; i++) {
       ptr_[i] = -(ptr_[i]);
     }
+
+    return *this;
   }
 
-  Mat& transpose() & { return *this; }
+  Mat& transpose() & {
+    try {
+      if (rows_ != cols_) {
+        throw "rows != cols";
+      }
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << std::endl;
+    }
 
-  void dump(std::ostream& os) const {}
+    int size = rows_;
+
+    for (int y = 0; y < size; y++) {
+      for (int x = y; x < size; x++) {
+        std::swap(ptr_[y * size + x], ptr_[x * size + y]);
+      }
+    }
+    return *this;
+  }
+
+  bool transpose(Mat& transposed_mat) const {
+    if (transposed_mat.nrows() != cols_ || transposed_mat.ncols() != rows_) {
+      return false;
+    }
+
+    for (int y = 0; y < rows_; y++) {
+      for (int x = 0; x < cols_; x++) {
+        transposed_mat.at(x, y) = ptr_[y * cols_ + x];
+      }
+    }
+
+    return true;
+  }
+
+  void dump(std::ostream& os) const { os << *this; }
+
+  void dump(std::ofstream& ofs) const { ofs << *this; }
+
+  bool dump(const std::string& fname) const {
+    std::ofstream ofs(fname, std::ios::out);
+    if (!ofs.is_open()) {
+      return false;
+    }
+    ofs << *this;
+    ofs.close();
+    return true;
+  }
 
   ProxyRow operator[](int n) {
     ProxyRow prow{.row = &ptr_[n * cols_]};
@@ -126,17 +174,104 @@ class Mat {
     ProxyRow prow{.row = &ptr_[n * cols_]};
     return prow;
   }
+
+  T& at(int y, int x) & {
+    try {
+      if (y >= rows_ || y < 0) {
+        throw "y - incorrect";
+      }
+
+      if (x >= cols_ || x < 0) {
+        throw "x - incorrect";
+      }
+
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << std::endl;
+    }
+
+    return ptr_[y * cols_ + x];
+  }
+
+  static Mat CreateEye(int rows, int cols) {
+    try {
+      if (rows != cols) {
+        throw "rows != cols";
+      }
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << std::endl;
+    }
+
+    Mat eye(rows, cols, static_cast<T>(0));
+
+    for (int i = 0; i < rows; i++) {
+      eye[i][i] = static_cast<T>(1);
+    }
+    return eye;
+  }
 };
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const la::Mat<T>& mat) {
   for (int y = 0; y < mat.nrows(); y++) {
     for (int x = 0; x < mat.ncols(); x++) {
+      if (std::is_same<T, unsigned long>::value ||
+          std::is_same<T, long>::value) {
+        os << std::dec << std::setw(14);
+      }
+
+      if (std::is_same<T, int>::value || std::is_same<T, short>::value ||
+          std::is_same<T, char>::value ||
+          std::is_same<T, unsigned int>::value ||
+          std::is_same<T, unsigned short>::value ||
+          std::is_same<T, unsigned char>::value) {
+        os << std::dec << std::setw(7);
+      }
+
+      if (std::is_same<T, float>::value) {
+        os << std::setw(10) << std::setprecision(4);
+      }
+
+      if (std::is_same<T, double>::value) {
+        os << std::setw(16) << std::setprecision(7);
+      }
+
       os << mat[y][x] << " ";
     }
     os << std::endl;
   }
   return os;
+}
+
+template <typename T>
+std::ofstream& operator<<(std::ofstream& ofs, const la::Mat<T>& mat) {
+  for (int y = 0; y < mat.nrows(); y++) {
+    for (int x = 0; x < mat.ncols(); x++) {
+      if (std::is_same<T, unsigned long>::value ||
+          std::is_same<T, long>::value) {
+        ofs << std::dec << std::setw(14);
+      }
+
+      if (std::is_same<T, int>::value || std::is_same<T, short>::value ||
+          std::is_same<T, char>::value ||
+          std::is_same<T, unsigned int>::value ||
+          std::is_same<T, unsigned short>::value ||
+          std::is_same<T, unsigned char>::value) {
+        ofs << std::dec << std::setw(7);
+      }
+
+      if (std::is_same<T, float>::value) {
+        ofs << std::setw(10) << std::setprecision(4);
+      }
+
+      if (std::is_same<T, double>::value) {
+        ofs << std::setw(16) << std::setprecision(7);
+      }
+
+      ofs << mat[y][x] << " ";
+    }
+    ofs << std::endl;
+  }
+  return ofs;
 }
 
 }  // namespace la
